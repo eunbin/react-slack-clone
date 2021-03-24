0f709f2d044f48f58c7c08b87cbe6a8bdac8abb2
eunbin <ahneunbin@gmail.com>
2021-03-24 23:20:30 +0900

    feat: react route 추가

--- front/src/layouts/App.tsx
+++ front/src/layouts/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { Switch, Redirect, Route } from 'react-router-dom';
+import Login from '@pages/login';
+import SignUp from '@pages/signup';
 
-const App = () => {
-  return <div>App</div>;
+const App: FC = () => {
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
 };
 
 export default App;
